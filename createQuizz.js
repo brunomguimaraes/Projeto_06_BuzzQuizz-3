3ac4d73d0c0fef71b212e19d43b2815ec9f14197
bruna <busouza01@gmail.com>
2021-08-13 23:01:33 -0500

    validada a tela 3.1, o inicício da criação do quizz

---
+++ createQuizz.js
@@ -0,0 +1,61 @@
+let quizzTitle, quizzImg = "";
+let quizzQuestions, quizzLevels = 0;
+
+function startCreateQuizzes() {
+    const disable = document.querySelector('.userQuizzes');
+    disable.classList.add('hide-class');
+}
+
+function validateUrl(string) {  
+    try {
+        url = new URL(string);
+      } catch (_) {
+        return false; 
+    }
+    return true;
+}
+
+function validateCreateQuizzes() {
+
+    if (quizzImg === '' || quizzTitle === '' || quizzLevels === '' || quizzQuestions === '') {
+        alert ('Preencha todos os campos por favor');
+        return false;
+    }
+    else {
+        quizzLevels = Number (quizzLevels);
+        quizzQuestions = Number (quizzQuestions);
+        if (isNaN(quizzLevels) || isNaN(quizzQuestions)) {
+            alert ('As perguntas e níveis devem ser números');
+            return false;
+        }
+        if (quizzTitle.length < 20 || quizzTitle.length > 65) {
+            alert ('O título deve conter no mínimo 20 e no máximo 65 caracteres ');
+            return false;
+        }
+        if (!validateUrl(quizzImg)) {
+            alert ('URL inválida')
+            return false;
+        }
+        //validar a url para por ! aqui 
+        if (quizzQuestions < 3) {
+            alert ('O quizz deve conter no mínimo 3 perguntas');
+            return false;
+        }
+        if (quizzLevels < 2) {
+            alert ('O quizz deve conter no mínimo 2 níveis')
+            return false;
+        }
+    } return true;
+}
+
+function finishCreateQuizzes() {
+    quizzTitle = document.getElementById('isTitle').value;
+    quizzImg = document.getElementById('isImg').value;
+    quizzQuestions = document.getElementById('isQuestions').value;
+    quizzLevels = document.getElementById('isLevels').value; 
+
+    if(validateCreateQuizzes() ) {
+        //chamar a próxima página 
+        alert ('deu certooo')
+    }
+}
